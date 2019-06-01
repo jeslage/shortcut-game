50afd08457faf4891b69a5d4fd2be22a31409612
Johannes Eslage <hello@johanneseslage.de>
2019-06-01 21:25:00 +0200

    Added touchDevice check

--- src/App.js
+++ src/App.js
@@ -1,37 +1,37 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from 'react';
 
-import Start from "./views/Start";
-import Game from "./views/Game";
-import Result from "./views/Result";
+import Start from './views/Start';
+import Game from './views/Game';
+import Result from './views/Result';
 
-import { SettingsContext } from "./context/SettingsProvider";
-import { useWindowEvent } from "./utils/hooks";
+import { SettingsContext } from './context/SettingsProvider';
+import { useWindowEvent } from './utils/hooks';
+import isTouchDevice from './utils/isTouchDevice';
 
 const App = () => {
   const { settings } = useContext(SettingsContext);
   const { view } = settings;
 
-  const [isMobile, setIsMobile] = useState(false);
+  const [touchDevice, setTouchDevice] = useState(false);
 
-  const checkViewportSize = () => {
-    if (window.innerWidth < 1024) {
-      setIsMobile(true);
-    } else if (isMobile === true) {
-      setIsMobile(false);
+  const touchDeviceCheck = () => {
+    if (isTouchDevice()) {
+      setTouchDevice(true);
+    } else {
+      setTouchDevice(false);
     }
   };
 
   useEffect(() => {
-    checkViewportSize();
+    touchDeviceCheck();
   }, []);
 
-  useWindowEvent("resize", () => checkViewportSize());
+  useWindowEvent('resize', () => touchDeviceCheck());
+
   return (
     <div className="App">
-      {isMobile ? (
-        <h2>
-          You need a bigger screen and a keyboard to play this game! Sorry...
-        </h2>
+      {touchDevice ? (
+        <h2>This game is not for touch devices.</h2>
       ) : (
         <>
           {view === 2 && <Game />}
